22bfcbd4a890e389ff63b4978dc2155233aa0254
quynhter <super007931999@gmail.com>
2026-04-22 15:54:13 +0300

    Change app.py

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ import sys
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QScrollArea,
     QWidget, QVBoxLayout, QHBoxLayout,
-    QPushButton
+    QPushButton, QComboBox, QLineEdit
 )
 from PySide6.QtCore import Qt
+
+def header_set(central_layout):
+    edit = QLineEdit()
+    edit.setPlaceholderText("Введите текст")
+    central_layout.addWidget(edit)
+    return central_layout
+
+def content_set(content_layout):
+    for i in range(30):
+        content_layout.addWidget(QPushButton(f"Кнопка {i}"))
+    return content_layout
+
+def footer_set(footer_layout):
+    edit = QLineEdit()
+    edit.setPlaceholderText("Введите текст")
+    footer_layout.addWidget(edit)
+    return footer_layout
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -21,6 +38,7 @@ class MainWindow(QMainWindow):
         
         header = QWidget()
         header_layout = QHBoxLayout()
+        header_layout = header_set(header_layout)
         header.setLayout(header_layout)
         central_layout.addWidget(header)
         
@@ -32,10 +50,8 @@ class MainWindow(QMainWindow):
         
         content = QWidget()
         content_layout = QVBoxLayout()
+        content_layout = content_set(content_layout)
         content.setLayout(content_layout)
-
-        for i in range(30):
-            content_layout.addWidget(QPushButton(f"Кнопка {i}"))
         
         body_scroll.setWidget(content)
         body_layout.addWidget(body_scroll)
@@ -45,6 +61,7 @@ class MainWindow(QMainWindow):
 
         footer = QWidget()
         footer_layout = QHBoxLayout()
+        footer_layout = footer_set(footer_layout)
         footer.setLayout(footer_layout)
         central_layout.addWidget(footer)
 
